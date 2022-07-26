69a40bb94e22a968357e303ac3b1f1a51639a2bd
moyi.li <moyi.li@vipshop.com>
2022-07-26 15:26:50 +0800

    feat: add expect

--- index.bash
+++ index.bash
@@ -9,6 +9,32 @@ echo "please connect to network first"
 exit 0
 fi
 echo "install expect to do more"
-sudo pacman -Sy expect
-echo "current Env:"
-echo $SYS_ENV
+echo Y | sudo pacman -Sy expect
+echo "current Env: $SYS_ENV"
+
+echo "start initialize disk"
+lsblk
+echo "input the disk you want to use"
+read DISK_NAME
+echo "you select $DISK_NAME, are you sure you want to format it?"
+if [ $? != "Y" ]; then
+echo "please rerun this shell script"
+exit 0
+fi
+expect<<-EOF
+spawn fdisk /dev/$DISK_NAME               // run process and trace it
+expect {
+"get help" {send "n\n";exp_continue}      // capture the output of spawn, and input a string to the process if it matches the expected character
+"default p" {send "p\n";exp_continue}     // N is the new partition, \n is enter, P is the main partition, and then enter
+"default 1" {send "1\n";exp_continue}        // partition number
+"default 2048" {send "\n";exp_continue}    // partition size
+"+size" {send "\n"}
+}
+
+expect "get help" {send "p\n";send "wq\n";exp_continue}   //p显示已建分区，wq保存配置，注意exp_continue，否则无法继续输入wq
+EOF
+mkdir /data    //新建挂载目录
+fdisk -l   
+mkfs.xfs /dev/$DISK_NAME   //格式化
+mount /dev/$DISK_NAME /data    //挂载使用
+df -Th
