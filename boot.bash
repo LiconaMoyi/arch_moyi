2a8f5d0619cb79be969ad269e9ea69baf24ddcb8
moyi.li <moyi.li@vipshop.com>
2022-10-04 17:06:08 +0800

    xx

--- boot.bash
+++ boot.bash
@@ -30,8 +30,8 @@ case $FORMAT_SELECTION in
         BOOT_SIZE="512M"
         MemTotal=$(cat /proc/meminfo |grep 'MemTotal' |awk -F : '{print $2}' |sed 's/^[ \t]*//g')
         SWAP_SIZE=$MemTotal
-        DISK_SIZE=fdisk -l | grep "Disk ${PARTDISK}" | awk -F , '{print $1}' | awk -F : '{print $2}' | sed 's/[ ]*//g' | awk -F G '{print $1}'
-        DISK_INFO=fdisk -l | grep "Disk ${PARTDISK}" | awk -F , '{print $1}' | awk -F : '{print $2}' | sed 's/[ ]*//g'
+        DISK_SIZE=$(fdisk -l | grep "Disk ${PARTDISK}" | awk -F , '{print $1}' | awk -F : '{print $2}' | sed 's/[ ]*//g' | awk -F G '{print $1}')
+        DISK_INFO=$(fdisk -l | grep "Disk ${PARTDISK}" | awk -F , '{print $1}' | awk -F : '{print $2}' | sed 's/[ ]*//g')
         echo "disk_size: $DISK_SIZE, disk_unit: $DISK_UNIT"
         HOME_SIZE=`echo "$DISK_SIZE * 0.7" | bc`${DISK_INFO:0-3}
         ROOT_SIZE=`echo "$DISK_SIZE * 0.3" | bc`${DISK_INFO:0-3}
