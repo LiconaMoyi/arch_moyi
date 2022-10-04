dd0edb7ca0a60fce11a0bab3cbd5d420d6d29432
moyi.li <moyi.li@vipshop.com>
2022-10-04 17:04:12 +0800

    xx

--- boot.bash
+++ boot.bash
@@ -31,7 +31,7 @@ case $FORMAT_SELECTION in
         MemTotal=$(cat /proc/meminfo |grep 'MemTotal' |awk -F : '{print $2}' |sed 's/^[ \t]*//g')
         SWAP_SIZE=$MemTotal
         DISK_SIZE=fdisk -l | grep "Disk ${PARTDISK}" | awk -F , '{print $1}' | awk -F : '{print $2}' | sed 's/[ ]*//g' | awk -F G '{print $1}'
-        DISK_INFO=fdisk -l | grep "Disk ${PARTDISK}" | awk -F , '{print $1}' | awk -F : '{print $2}' | sed 's/[ ]*//g')
+        DISK_INFO=fdisk -l | grep "Disk ${PARTDISK}" | awk -F , '{print $1}' | awk -F : '{print $2}' | sed 's/[ ]*//g'
         echo "disk_size: $DISK_SIZE, disk_unit: $DISK_UNIT"
         HOME_SIZE=`echo "$DISK_SIZE * 0.7" | bc`${DISK_INFO:0-3}
         ROOT_SIZE=`echo "$DISK_SIZE * 0.3" | bc`${DISK_INFO:0-3}
