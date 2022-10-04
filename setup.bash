2f468d824480eaca194e03692762a322d165f67f
moyi.li <moyi.li@vipshop.com>
2022-10-04 19:42:26 +0800

    xx

--- setup.bash
+++ setup.bash
@@ -28,9 +28,9 @@ echo "set ${USER_NAME} password"
 passwd ${USER_NAME}
 sed -i '/^root ALL=(ALL:ALL) ALL/a licona ALL=(ALL:ALL) ALL' /etc/sudoers
 
-#echo "Y" | pacman -Sy dosfstools grub efibootmgr
-#grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=Arch linux
-#grub-mkconfig -o /boot/grub/grub.cfg
+echo "Y" | pacman -Sy dosfstools grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=Arch linux
+grub-mkconfig -o /boot/grub/grub.cfg
 echo "Y" | sudo pacman -Sy iwd
 systemctl enable iwd
 exit
