18aceb912573c947cc73dcee49212cfc8cecfdc3
moyi.li <moyi.li@vipshop.com>
2022-10-04 19:27:32 +0800

    xx

--- test.bash
+++ test.bash
@@ -1,7 +1,7 @@
 #! /bin/bash
-echo "Y" | pacman -Sy dosfstools grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=Arch linux
-grub-mkconfig -o /boot/grub/grub.cfg
+#echo "Y" | pacman -Sy dosfstools grub efibootmgr
+#grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=Arch linux
+#grub-mkconfig -o /boot/grub/grub.cfg
 echo "Y" | sudo pacman -Sy iwd
 systemctl enable iwd
 exit
--- test/utils/base_test.bash
+++
@@ -1,7 +0,0 @@
-source ../../utils/base.bash
-network
-if [ $? -eq 0 ];then
-	echo "please connect to network first"
-	exit -1
-else echo "connect to network successfully"
-fi
