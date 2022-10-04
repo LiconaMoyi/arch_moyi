892e7f650266211246db303caa34c1f1b7234544
moyi.li <moyi.li@vipshop.com>
2022-10-04 19:32:40 +0800

    xx

--- setup.bash
+++ setup.bash
@@ -14,13 +14,8 @@ echo "
 ::1 localhost
 127.0.0.1 $HOST_NAME.localdomain $HOST_NAME
 " > /etc/hosts
-echo "input the root password you want to use:"
-read ROOT_PASSWORD
-expect<<-EOF
+echo "set root password"
 passwd
-expect "New password" {send "${ROOT_PASSWORD}\n";exp_continue}
-expect "Retype new password" {send "${ROOT_PASSWORD}\n";}
-EOF
 echo "set root password successfully"
 echo "input the group name you want to use:"
 read GROUP_NAME
@@ -32,3 +27,14 @@ useradd $USER_NAME -g $GROUP_NAME -m
 echo "set ${USER_NAME} password"
 passwd ${USER_NAME}
 sed -i '/^root ALL=(ALL:ALL) ALL/a licona ALL=(ALL:ALL) ALL' /etc/sudoers
+
+#echo "Y" | pacman -Sy dosfstools grub efibootmgr
+#grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=Arch linux
+#grub-mkconfig -o /boot/grub/grub.cfg
+echo "Y" | sudo pacman -Sy iwd
+systemctl enable iwd
+exit
+sleep 3
+exit
+umount -R /mnt
+reboot
--- test.bash
+++ test.bash
@@ -1,11 +1,1 @@
 #! /bin/bash
-#echo "Y" | pacman -Sy dosfstools grub efibootmgr
-#grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=Arch linux
-#grub-mkconfig -o /boot/grub/grub.cfg
-echo "Y" | sudo pacman -Sy iwd
-systemctl enable iwd
-exit
-sleep 3
-exit
-umount -R /mnt
-reboot
